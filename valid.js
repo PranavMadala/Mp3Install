1fc13af9263a27c43e42b6ef38a750e98a83da4c
PranavMadala <165781065+PranavMadala@users.noreply.github.com>
2024-04-14 20:09:55 +0530

    Add files via upload

---
+++ valid.js
@@ -0,0 +1,27 @@
+function show1password() {
+	var show =document.getElementById("password");
+	if (show.type=='password') {
+	show.type='text'
+
+	}
+	else{
+	show.type='password'
+	}
+
+}
+function showpassword() {
+	var show =document.getElementById("confirm");
+	if (show.type=='confirm') {
+	show.type='text'
+
+	}
+	else{
+	show.type='confirm'
+	}
+}
+function password(){
+	document.HTMLelement.text("password").inner.html
+}
+function confirm(){
+	document.HTMLelement.text("confirm").inner.html
+}
